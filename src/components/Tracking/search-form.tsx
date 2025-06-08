56652964a295e8f189e4f022d5aa41761d8ac5ab
yosho <yleecruz@gmail.com>
2025-06-07 20:43:14 -0400

    details

--- src/components/Tracking/search-form.tsx
+++ src/components/Tracking/search-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+//import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { useFetchByInvoiceOrHBL } from "../../hooks/useFetchByInvoiceOrHBL";
 import TrackingDetails from "./TrackingDetails";
