34b615b520bb453c2f29ac699501a4e5c381fb99
yosho <yleecruz@gmail.com>
2025-06-18 19:31:38 -0400

    added tracking button

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,8 +1,8 @@
 export const heroDetails = {
-	heading: "Tu vía rápida y segura para preparar documentos legales en EE.UU.",
+	heading:
+		"Tu vía rápida y segura para preparar documentos legales y envío de paquetes a Cuba desde EE.UU",
 	subheading:
-		"Orlen Escalona – Preparadora de formularios y documentos legales en Estados Unidos. Te asistimos con la organización y entrega de tus trámites migratorios y civiles.",
-	subheading2:
-		"No somos abogados y no damos asesoría",
+		"Orlen Escalona – Preparadora de formularios y documentos legales en Estados Unidos.Te asistimos con la organización y entrega de tus trámites migratorios y civiles. Además, ofrecemos envío de paquetes a Cuba, con seguimiento completo hasta la puerta de tu hogar.",
+	subheading2: "No somos abogados y no damos asesoría",
 	centerImageSrc: "/images/rapidvia-services1.webp",
 };
