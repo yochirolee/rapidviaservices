42d3c2175d8cf7854233df295da459a8d42ba444
yosho <yleecruz@gmail.com>
2025-06-18 20:46:38 -0400

    added tracking button

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,7 +1,7 @@
 export const heroDetails = {
 	heading: "Documentos legales y envío de paquetes a Cuba desde EE.UU.",
 	subheading:
-		"En RapidVia Services, tu vía rápida y segura, te asistimos en la preparación de documentos para trámites legales y migratorios. También realizamos envíos a Cuba con seguimiento hasta la puerta de tu hogar.",
+		"En RapidVia Services, tu vía rápida y segura, te asistimos en la preparación de documentos para trámites legales y migratorios. También realizamos envío de paquetes a Cuba con seguimiento hasta la puerta de tu hogar.",
 	subheading2: "No somos abogados y no damos asesoría",
 	centerImageSrc: "/images/rapidvia-services1.webp",
 };
