27ddf1d3b0292c45943827ad7fd97ed80b5a4e61
Leidiana Torres Roca <yleecruz@gmail.com>
2025-10-01 16:32:23 -0400

    last fixed

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -13,7 +13,6 @@ const OurServices = () => {
       <div className="absolute left-0 top-0 bottom-0 -z-10 w-full">
         <div className="absolute inset-0 h-full w-full bg-red-800 bg-gradient-to-r from-gray-200 to-gray-300 bg-[size:40px_40px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"></div>
       </div>
-
       <div className="container mx-auto">
         {/* Encabezado */}
         <div className="flex flex-wrap items-stretch">
